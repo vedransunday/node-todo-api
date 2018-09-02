8e2243444661ee095615d148e159672c2f54193b
VEDRAN PERENDIJA <vedranperendija@VEDRANs-MacBook-Pro.local>
2018-09-02 11:25:15 +0200

    changed

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
